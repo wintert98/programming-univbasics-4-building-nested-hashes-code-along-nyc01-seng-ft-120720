45d295424e2a721d81b2bc8a7e20a054ba03f863
Thomas Winter <wintert98@gmail.com>
2020-11-26 19:18:24 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -47,14 +47,14 @@ def third_challenge
         },
       :heroine_friends => [
         {
-          :name => "Steven",
-          :age => "30", 
-          :attitude => "confused"
+           name: "Steven",
+           age: "30", 
+           attitude: "confused"
         },
         {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
+           name: "Nurse",
+           age: "44",
+           attitude: "worried"
         }
         ]
    }
