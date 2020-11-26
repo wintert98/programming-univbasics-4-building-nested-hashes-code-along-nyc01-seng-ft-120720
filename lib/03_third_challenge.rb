33e0609b64950f333a1f89067353c8d7d38092a5
Thomas Winter <wintert98@gmail.com>
2020-11-26 19:22:49 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -18,8 +18,8 @@ def third_challenge
          :status => "alive"
         },
       :hero_friends => [
-        { name: "Benvolio", age: "17", attitude: "worried"},
-        { name: "Mercutio", age: "18", attitude: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
