907e17fec8d73d5fa7b08f9b9c8cc0d47efa2e42
Thomas Winter <wintert98@gmail.com>
2020-11-26 19:10:21 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -23,7 +23,7 @@ def third_challenge
          :attitude =>  "worried"
         },
         {:name => "Mercutio",
-         :age =>
+         :age => "18",
          :attitude =>
        }
         ]
